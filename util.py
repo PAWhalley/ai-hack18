2d712a8a874f05aa782ca74fd5991348c18f553b
Tyler Farghly <tylerf98@live.co.uk>
2018-11-17 21:14:59 +0000

    Time is working

--- util.py
+++ util.py
@@ -3,15 +3,14 @@ import numpy as np
 
 def read_with_timestamps(path):
 	data = pd.read_csv(path, sep=",")
-	timestamps = pd.read_csv("timestamp.csv", sep=",")
+	timestamps = pd.read_csv("timestamps.csv", sep=",")
 
 	data.insert(0, "timestamp", timestamps)
 
 	return data
 
 def remove_missing(df, feature):
-	missing = np.logical_and(np.isnan(df[feature]), np.less(df[feature], 0))
-
-	df = df[np.logical_not(missing)]
+	df = df[np.logical_not(np.isnan(df[feature]))]
+	df = df[np.logical_not(np.less(df[feature], 0))]
 
 	return df
