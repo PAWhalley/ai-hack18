89f72a89e78ad7c7d960349d0c6da984ff209614
raymess <44314205+raymess@users.noreply.github.com>
2018-11-18 01:33:07 +0000

    Update spectral_analysis.py

--- spectral_analysis.py
+++ spectral_analysis.py
@@ -3,8 +3,10 @@ import pandas
 import numpy as np
 import matplotlib.pyplot as plt
 import util
-import time_histogram as th
+import time_series_analysis as ts
 import os
+from scipy.fftpack import fft
+
 
 #set the value of the bin
 std_bin_size = 60*60*24
@@ -17,8 +19,18 @@ data = util.read_with_timestamps("Road-Accident.csv")
 os.chdir(r"C:\Users\REMY\Documents\GitHub\ai-hack18")
 
 ## plotting
-print(type(data))
-x,y,z = th.time_histogram("age_of_driver", data)
+x, y = ts.histogram(data, "age_of_driver", value=36)
 
-plt.plot(x,z)
+
+## computing the mean and reducing from the data to obtain mean zero new data
+mean_y = ts.stationary_mean(y)
+y_0 = y - mean_y
+plt.plot(x, y_0)
+plt.show()
+
+
+## fourier transform
+fy = fft(y_0)
+fx = np.linspace(0.0, 300, 364)
+plt.plot(fx, fy)
 plt.show()
